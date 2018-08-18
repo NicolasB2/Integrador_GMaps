1fdf86683f80c9d0453e4cf111f955c5933b8b07
JuanManu59 <juan5.9@hotmail.com>
2018-08-18 12:16:37 -0500

    create the research center class with their respective attributes, constructors and necessary methods

---
+++ WindowsFormsApp1/WindowsFormsApp1/CentroInvestigacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class CentroInvestigacion
+    {
+
+        NME_CONVOCATORIA
+            ANO_CONVO
+            COD_GRUPO_GR
+            NME_GRUPO_GR
+            FCREACION_GR
+            NME_MUNICIPIO_GR
+            NME_DEPARTAMENTO_GR
+            NME_PAIS_GR
+            NME_REGION_GR
+            COD_DANE_GR
+            ID_AREA_CON_GR
+            NME_AREA_GR
+            NME_GRAN_AREA_GR
+            NME_CLASIFICACION_GR
+            ORDEN_CLAS_GR
+            EDAD_ANOS_GR
+    }
+}
