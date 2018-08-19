e36d45792d2adfd21ca28a4320b3412f8c26e130
Sara Ortiz Drada <38886724+saradrada@users.noreply.github.com>
2018-08-19 10:56:17 -0500

    Add attributes to the App class.

--- WindowsFormsApp1/WindowsFormsApp1/App.cs
+++ WindowsFormsApp1/WindowsFormsApp1/App.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
+
+     
+
     class App
     {
+        private List<InvestigationGroup> groupList;
+        private Report report;
+
+        //This attribute indicates if the any group on the list has been updated. 
+        private bool listChanged; 
+
     }
 }
