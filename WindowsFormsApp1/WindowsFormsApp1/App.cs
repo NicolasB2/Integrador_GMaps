d6ae56940287d399f4c582dc81b90f1dac9b4c07
Sara Ortiz Drada <38886724+saradrada@users.noreply.github.com>
2018-08-19 11:07:42 -0500

    Add relations and methods declarations to App class.

--- WindowsFormsApp1/WindowsFormsApp1/App.cs
+++ WindowsFormsApp1/WindowsFormsApp1/App.cs
@@ -8,9 +8,36 @@ using System.Threading.Tasks;
 namespace WindowsFormsApp1
 {
 
-     
+
 
     class App
     {
+        public List<InvestigationGroup> GroupList { set; get; }
+        public Report Report { set; get; }
+
+        //This tells me if any of the groups on the list has been updated. 
+        public bool ListUpdated { set; get; }
+
+
+        public void RegisterGroup(string[] groupAttributes)
+        {
+
+        }
+
+        public void UpdateGroup(string[] groupAttributes)
+        {
+
+        }
+
+        public void GetGroupInfo(InvestigationGroup iG)
+        {
+
+        }
+
+        public void getGroupsReport(string filter)
+        {
+
+        }
+
     }
 }
