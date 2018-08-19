83b3ccb80bd4a939202884ae57ca60113948bebe
Nicolas <Nicolas@BLUESKY>
2018-08-19 13:02:26 -0500

    Continuation of the App class

--- WindowsFormsApp1/WindowsFormsApp1/App.cs
+++ WindowsFormsApp1/WindowsFormsApp1/App.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Collections;
 using System.Threading.Tasks;
@@ -8,28 +9,40 @@ using System.Threading.Tasks;
 namespace WindowsFormsApp1
 {
 
-
-
     class App
     {
+        //ATTRIBUTES & CONSTANTS ****************************************************************
+
+        //This tells me the relative path of file whit the infotmation of the groups
+        public static String file = "";
+
+        //Data Structure whit all the information of group list
         public List<InvestigationGroup> GroupList { set; get; }
+
+        //This tells me if any of the groups on the list has been updated. 
         public Report Report { set; get; }
-        public String file { set; get; }
 
         //This tells me if any of the groups on the list has been updated. 
         public bool ListUpdated { set; get; }
 
 
+        //METHODS*******************************************************************************
+
+        //this add the new investigation group in the file and GroupList
         public void RegisterGroup(string[] groupAttributes)
         {
             InvestigationGroup group = new InvestigationGroup(groupAttributes);
+            GroupList.Add(group);
+            ListUpdated = true;
         }
 
+        //
         public void UpdateGroup(String selectedGroup, string[] groupAttributes)
         {
 
         }
 
+        //this method find and retorn the InvestigationGroup with the name of the parameter
         public InvestigationGroup GetGroupInfo(String nombre)
         {
               IEnumerable <InvestigationGroup> x =  from n in GroupList
@@ -39,6 +52,7 @@ namespace WindowsFormsApp1
             return x.ElementAt(0);
         }
 
+        //
         public void getGroupsReport(string filter)
         {
 
