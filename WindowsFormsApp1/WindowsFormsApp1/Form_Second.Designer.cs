e2a16770807eb92dd8add1938240a3bc76d5c7f8
Nicolas <Nicolas@BLUESKY>
2018-08-18 21:56:05 -0500

    Add panels and userforms to GUI.

--- WindowsFormsApp1/WindowsFormsApp1/Form_Second.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form_Second.Designer.cs
@@ -28,19 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.uC_GroupInfo1 = new WindowsFormsApp1.UC_GroupInfo();
+            this.uC_GroupList2 = new WindowsFormsApp1.UC_GroupList();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.uC_Report1 = new WindowsFormsApp1.UC_Report();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.uC_GroupList2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(595, 313);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.uC_GroupInfo1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(595, 313);
+            this.panel2.TabIndex = 1;
+            // 
+            // uC_GroupInfo1
+            // 
+            this.uC_GroupInfo1.Location = new System.Drawing.Point(188, 46);
+            this.uC_GroupInfo1.Name = "uC_GroupInfo1";
+            this.uC_GroupInfo1.Size = new System.Drawing.Size(189, 150);
+            this.uC_GroupInfo1.TabIndex = 0;
+            // 
+            // uC_GroupList2
+            // 
+            this.uC_GroupList2.Location = new System.Drawing.Point(114, 27);
+            this.uC_GroupList2.Name = "uC_GroupList2";
+            this.uC_GroupList2.Size = new System.Drawing.Size(354, 250);
+            this.uC_GroupList2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.uC_Report1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(595, 313);
+            this.panel3.TabIndex = 1;
+            // 
+            // uC_Report1
+            // 
+            this.uC_Report1.Location = new System.Drawing.Point(200, 68);
+            this.uC_Report1.Name = "uC_Report1";
+            this.uC_Report1.Size = new System.Drawing.Size(177, 150);
+            this.uC_Report1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 313);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private UC_GroupInfo uC_GroupInfo1;
+        private UC_GroupList uC_GroupList2;
+        private System.Windows.Forms.Panel panel3;
+        private UC_Report uC_Report1;
     }
 }
---
+++ WindowsFormsApp1/WindowsFormsApp1/UC_Report.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class UC_Report
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // UC_Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Name = "UC_Report";
+            this.Size = new System.Drawing.Size(177, 150);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
