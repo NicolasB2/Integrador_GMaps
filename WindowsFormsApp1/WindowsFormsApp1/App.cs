3790e489f048755fb0e236a1f04428d4b325c389
Sara Ortiz Drada <38886724+saradrada@users.noreply.github.com>
2018-08-19 11:00:23 -0500

    Test commit.

--- WindowsFormsApp1/WindowsFormsApp1/App.cs
+++ WindowsFormsApp1/WindowsFormsApp1/App.cs
@@ -2,37 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
+
+     
+
     class App
     {
-        public bool Changed { get; set; }
-        public List<InvestigationGroup> Groups { get; set; }
-
-
-        App()
-        {
-        }
-
-        public void registerGroup(string[] data)
-        {
-        }
-
-        public void updateGroup(string[] data)
-        {
-        }
-
-        public void getGroupInformation(InvestigationGroup ig)
-        {
-        }
-
-        public void getGroupsReport(string data)
-        {
-        }
-
-
     }
-
 }
