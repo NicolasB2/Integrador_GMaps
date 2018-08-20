3c1a2fe9e7204e88d6d9a07083cf18430094470a
Sara Ortiz Drada <38886724+saradrada@users.noreply.github.com>
2018-08-20 16:06:13 -0500

    (Re)Add components on Form_Second.

--- WindowsFormsApp1/WindowsFormsApp1/Form_Second.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form_Second.Designer.cs
@@ -31,13 +31,56 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent(App program)
         {
+
+            this.panel1 = new System.Windows.Forms.Panel();
+
+            this.uC_Report1 = new WindowsFormsApp1.UC_Report(program);
+            this.uC_GroupList1 = new WindowsFormsApp1.UC_GroupList(this);
+            this.uC_GroupInfo1 = new WindowsFormsApp1.UC_GroupInfo();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.uC_Report1);
+            this.panel1.Controls.Add(this.uC_GroupList1);
+            this.panel1.Controls.Add(this.uC_GroupInfo1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(711, 441);
+            this.panel1.TabIndex = 0;
+            // 
+            // uC_Report1
+            // 
+            this.uC_Report1.Location = new System.Drawing.Point(12, 3);
+            this.uC_Report1.Name = "uC_Report1";
+            this.uC_Report1.Size = new System.Drawing.Size(690, 426);
+            this.uC_Report1.TabIndex = 2;
+            // 
+            // uC_GroupList1
+            // 
+            this.uC_GroupList1.Location = new System.Drawing.Point(163, 38);
+            this.uC_GroupList1.Name = "uC_GroupList1";
+            this.uC_GroupList1.Size = new System.Drawing.Size(350, 400);
+            this.uC_GroupList1.TabIndex = 1;
+            // 
+            // uC_GroupInfo1
+            // 
+            this.uC_GroupInfo1.Location = new System.Drawing.Point(12, 3);
+            this.uC_GroupInfo1.Name = "uC_GroupInfo1";
+            this.uC_GroupInfo1.Size = new System.Drawing.Size(690, 400);
+            this.uC_GroupInfo1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 441);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
